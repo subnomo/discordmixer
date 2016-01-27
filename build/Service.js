8dbdf41cb2074e01b9a851c7f726967532c4a7e1
Alex Taylor <alex@subnomo.me>
2016-01-26 18:31:51 -0800

    Update gitignore

--- build/Service.js
+++
@@ -1,8 +0,0 @@
-var Config = require('./utils/ParseConfig');
-var Service = (function () {
-    function Service() {
-        this.config = new Config();
-    }
-    return Service;
-})();
-module.exports = Service;
