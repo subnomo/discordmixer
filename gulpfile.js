eb537f11aaf188f4a6e3be5f0f3c0675951895e7
Alex Taylor <alex@subnomo.me>
2016-05-21 12:28:40 -0700

    Add new gulp task for creating releases

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var tslint = require('gulp-tslint');
 var watch = require('gulp-watch');
+var zip = require('gulp-zip');
+var rename = require('gulp-rename');
 
-gulp.task('ts-lint', function () {
-   return gulp.src("src/**/*.ts").pipe(tslint()).pipe(tslint.report('prose'));
+gulp.task('lint', function () {
+   return gulp.src('src/**/*.ts')
+        .pipe(tslint())
+        .pipe(tslint.report('prose'));
 });
 
-gulp.task('ts-compile', function () {
+gulp.task('build', function () {
     return gulp.src('src/**/*.ts')
         .pipe(ts({
             module: 'commonjs',
@@ -18,7 +22,25 @@ gulp.task('ts-compile', function () {
         .pipe(gulp.dest('build'));
 });
 
-gulp.task('default', ['ts-lint', 'ts-compile']);
+gulp.task('dist', ['build'], function () {
+    return gulp.src(['build/**/*', 'config.json.dist', 'package.json', 'README.md'],
+                    { base: './' })
+        .pipe(rename(function (path) {
+            var file = path.basename + path.extname;
+
+            // Only rename config.json.dist
+            if (file === 'config.json.dist') {
+                path.basename = 'config';
+                path.extname = '.json';
+            }
+
+            return path;
+        }))
+        .pipe(zip('discordmixer.zip'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['lint', 'build']);
 
 gulp.task('watch', function () {
    watch('src/**/*.ts', function () {
